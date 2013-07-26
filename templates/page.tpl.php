dce4c5783df1d6b43355904b54a33b36105170dc
Jared Whiklo <jared_whiklo@umanitoba.ca>
2013-07-26 14:40:06 +0000

    Fixes for the islandora collection search when displaying the dropdown menu. Also removal of some extra code in the templates.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -115,29 +115,7 @@
 
   <div id="main">
     <div id="navigation">
-
-      <?php if ($main_menu): ?>
-        <!--<nav id="main-menu" role="navigation">-->
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-         /* print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          ));*/ ?>
-        <!--</nav>-->
-      <?php endif; ?>
       <?php print render($page['navigation']); ?>
-      
     </div><!-- /#navigation -->
   
   
@@ -145,7 +123,7 @@
       <?php print render($page['searchbar']); ?>
     </div>
     <?php print $breadcrumb; ?>
-    <div id="content" class="column" role="main">
+    <div id="content" class="column<?php if (!$breadcrumb) { print " no-breadcrumb";}?>" role="main">
       <?php print render($page['highlighted']); ?>
       
       <a id="main-content"></a>
