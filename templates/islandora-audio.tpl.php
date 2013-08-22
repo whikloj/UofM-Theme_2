75cf11d8703b9cd2b11e0a3587b9e48cb8631a12
Jared Whiklo <jared_whiklo@umanitoba.ca>
2013-08-22 14:45:25 -0500

    Add title tracking to GA custom vars

--- templates/islandora-audio.tpl.php
+++ templates/islandora-audio.tpl.php
@@ -7,9 +7,11 @@
  * @TODO: add documentation about file and available variables
 *  @TODO: drupal_set_title shouldn't be called here
  */
+ $collection_pids = array(); // To hold parent collection pids for Google Analytics
 ?>
 
 <div class="islandora-audio-object islandora">
+  <?php print manidora_print_insert_link("print/islandora/object/{$islandora_object->id}"); ?>
   <div class="islandora-audio-content-wrapper clearfix">
     <?php if (isset($islandora_content)): ?>
       <div class="islandora-audio-content">
@@ -27,6 +29,7 @@
         <ul>
           <?php foreach ($parent_collections as $collection): ?>
             <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+             <?php $collection_pids[] = $collection->id; ?>
           <?php endforeach; ?>
         </ul>
       </div>
@@ -34,4 +37,11 @@
   </div>
   </div>
 </div>
-
+<script type="text/javascript">
+<!--
+  var _gaq = _gaq || [];
+  _gaq.push(['_setCustomVar', 1, 'PID', '<?php print $islandora_object->id;?>', 3]);
+  _gaq.push(['_setCustomVar', 2, 'Collection', '<?php print implode($collection_pids,'|');?>', 3]);
+  _gaq.push(['_setCustomVar', 3, 'Title', '<?php print $islandora_object_label;?>', 3]);
+//-->
+</script>
--- templates/islandora-video.tpl.php
+++ templates/islandora-video.tpl.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * This is the template file for the object page for large image
+ * This is the template file for the object page for video
  *
  * Available variables:
  * - $islandora_object: The Islandora object rendered in this template file
@@ -12,36 +12,33 @@
  * - $islandora_object_label: The sanitized object label.
  * - $parent_collections: An array containing parent collection(s) info.
  *   Includes collection object, label, url and rendered link.
- * - $islandora_thumbnail_img: A rendered thumbnail image.
- * - $islandora_content: A rendered image. By default this is the JPG datastream
- *   which is a medium sized image. Alternatively this could be a rendered
- *   viewer which displays the JP2 datastream image.
  *
- * @see template_preprocess_islandora_large_image()
- * @see theme_islandora_large_image()
+ * @see template_preprocess_islandora_video()
+ * @see theme_islandora_video()
  */
-  $collection_pids = array(); // To hold parent collection pids for Google Analytics
+ $collection_pids = array(); // To hold parent collection pids for Google Analytics
+ 
 ?>
 
-<div class="islandora-large-image-object islandora">
-<?php print manidora_print_insert_link("print/islandora/object/{$islandora_object->id}"); ?>
-  <div class="islandora-large-image-content-wrapper clearfix">
+<div class="islandora-video-object islandora" itemscope itemtype="http://schema.org/VideoObject">
+  <div class="islandora-video-content-wrapper clearfix">
     <?php if ($islandora_content): ?>
-      <?php if (isset($image_clip)): ?>
-        <?php print $image_clip; ?>
-      <?php endif; ?>
-      <div class="islandora-large-image-content">
+      <div class="islandora-video-content">
         <?php print $islandora_content; ?>
       </div>
     <?php endif; ?>
-  <div class="islandora-large-image-sidebar">
+  <div class="islandora-video-sidebar">
+    <?php if (!empty($dc_array['dc:description']['value'])): ?>
+      <h2><?php print $dc_array['dc:description']['label']; ?></h2>
+      <p itemprop="description"><?php print $dc_array['dc:description']['value']; ?></p>
+    <?php endif; ?>
     <?php if ($parent_collections): ?>
       <div>
         <h2><?php print t('In collections'); ?></h2>
         <ul>
           <?php foreach ($parent_collections as $collection): ?>
         <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
-         <?php $collection_pids[] = $collection->id; ?>
+          <?php $collection_pids[] = $collection->id; ?>
           <?php endforeach; ?>
         </ul>
       </div>
@@ -57,3 +54,4 @@
   _gaq.push(['_setCustomVar', 3, 'Title', '<?php print $islandora_object_label;?>', 3]);
 //-->
 </script>
+
