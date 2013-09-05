ae8f90f2ebdd89769a6b4b7fd7a664ec7fd330e3
Jared Whiklo <jared_whiklo@umanitoba.ca>
2013-09-05 09:35:37 -0500

    Making display of search results and collection pages similar in box size and JS expand function for increased screen real estate.

--- js/collection_page.js
+++ js/collection_page.js
@@ -1,11 +1,13 @@
 Drupal.behaviors.UofM_Theme_2 = {
   attach: function(context, settings) {
-    var windowH = jQuery(window).height();
-    var boxT = jQuery('.islandora-basic-collection-grid').offset().top;
-    var boxH = jQuery('.islandora-basic-collection-grid').height();
-    if ((boxT + boxH) < windowH){
-      new_height = windowH - boxT - 10; /* 10px to leave a small gap at the bottom */
-      jQuery('.islandora-basic-collection-grid').css('height',new_height + 'px');
+    if (jQuery('.islandora-basic-collection-grid, .islandora-solr-grid').length > 0){
+      var windowH = jQuery(window).height();
+      var boxT = jQuery('.islandora-basic-collection-grid, .islandora-solr-grid').offset().top;
+      var boxH = jQuery('.islandora-basic-collection-grid, .islandora-solr-grid').height();
+      if ((boxT + boxH) < windowH){
+        new_height = windowH - boxT - 10; /* 10px to leave a small gap at the bottom */
+        jQuery('.islandora-basic-collection-grid, .islandora-solr-grid').css('height',new_height + 'px');
+      }
     }
   }
-} 
+}
