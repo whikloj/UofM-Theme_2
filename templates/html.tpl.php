81c420cb490a1aaa43cd173e0cee6c9a966e9b14
Jared Whiklo <jared_whiklo@umanitoba.ca>
2013-09-17 12:24:32 -0500

    Adjust for mobile tablet (portrait) layout.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -83,7 +83,7 @@
   <?php if ($default_mobile_metatags): ?>
     <meta name="MobileOptimized" content="width">
     <meta name="HandheldFriendly" content="true">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
   <?php endif; ?>
   <!--[if IEMobile]>
   <meta http-equiv="cleartype" content="on">
