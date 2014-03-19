1c49d14c849086cdac453fb5dcd934e2ece6196b
Jared Whiklo <jared_whiklo@umanitoba.ca>
2014-03-19 11:50:00 -0500

    Clean up the page not found display

--- templates/page--404.tpl.php
+++ templates/page--404.tpl.php
@@ -132,7 +132,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <h1 class="title page-404" id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
