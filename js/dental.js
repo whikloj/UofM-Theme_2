c9a9340bca97a08354e667209e5a28c8e16b71d4
Jared Whiklo <jwhiklo@gmail.com>
2016-02-11 14:32:49 -0600

    Change search facet display Have search bar stick to top of the screen when scrolling Remove content resize and overflow scrolling

--- js/dental.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  jQuery(document).ready(function(){
-    jQuery('.uofm-dental-info h2').click(function(){ jQuery(this).next('table').toggle(function(){ var tmpT = (jQuery(this).is(':visible') ? 'hide' : 'show'); jQuery(this).prev('h2').text('Information (' + tmpT + ')'); });});
-  });
-})();
--- js/script.js
+++ js/script.js
@@ -24,52 +24,49 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function (jQuery, Drupal, window, document, undefined) {
 
-  jQuery(document).ready(function(){
-    // Setup up onclick handler for homepage tabs
-    jQuery('.islandora-solr-facet-wrapper h3').click(
-      function(){
-        var showIt = (jQuery(this).siblings().css('display') == 'none' ? 1 : 0);
-        jQuery('.islandora-solr-facet, .islandora-solr-range-slider, .islandora-solr-date-filter').hide();
-        jQuery('.islandora-solr-facet-wrapper h3').removeClass('expanded');
-        if (showIt){
-          jQuery(this).addClass('expanded').siblings('.islandora-solr-facet, .islandora-solr-range-slider, .islandora-solr-date-filter').show();
+  Drupal.behaviors.UofM_Theme_2 = {
+    attach: function(context, settings) {
+
+      // Make searchbar stick to the top of the screen.
+      var searchbarTop = jQuery('#searchbar').offset().top;
+      jQuery(window).scroll(function(){
+        //console.log(jQuery(window).scrollTop());
+        if (jQuery(window).scrollTop() > searchTop) {
+          jQuery('#searchbar').addClass('position-fixed');
+        } else if (jQuery(window).scrollTop() > searchTop) {
+          jQuery('#searchbar').removeClass('position-fixed');
         }
       });
-    jQuery('.islandora-solr-range-slider, .islandora-solr-date-filter').css('left',0).hide(); // Move FLOT graph over and hide now
-    // 2013-06-17 whikloj - 
-    //   Removing the selectBoxIt as it requires jQuery-UI 10 and that requires the jQuery Update module which 
-    //   causes problems with the Drupal overlay administration pages.
-    /*if (jQuery('#edit-collection-select').length > 0){
-      jQuery('#edit-collection-select').selectBoxIt({'theme':'bootstrap'});
-      jQuery('#edit-collection-select').change(function(){ 
-        jQuery("#edit-collection-select option").removeAttr("selected");
-        jQuery("#edit-collection-select option[value='" + jQuery('#edit-collection-selectSelectBoxItText').attr('data-val') + "']").attr('selected','selected');
-      });
-    }*/
-  });
 
-  // Scroll active compound into view.
-  jQuery(window).load(function(){ 
-    if (jQuery('.islandora-compound-thumbs, .islandora-compound-jail-thumbs').length > 0) {
-      var active = jQuery('.islandora-compound-thumb a.active:visible, .islandora-compound-object-jail-active:visible');
-      var activeLeft = jQuery(active).offset().left;
-      var wrapWidth = jQuery('.islandora-compound-thumbs-wrapper, .islandora-compound-jail-thumbs').width();
-      var wrapLeft = jQuery('.islandora-compound-thumbs-wrapper, .islandora-compound-jail-thumbs').offset().left;
-      if (activeLeft > (wrapLeft + wrapWidth)){
-        jQuery(active).scrollParent().animate({'scrollLeft': (activeLeft - wrapLeft - (wrapWidth / 2)) + 'px'},500);
+      // Scroll active compound into view.
+      if (jQuery('.islandora-compound-thumbs, .islandora-compound-jail-thumbs').length > 0) {
+        var active = jQuery('.islandora-compound-thumb a.active:visible, .islandora-compound-object-jail-active:visible');
+        var activeLeft = jQuery(active).offset().left;
+        var wrapWidth = jQuery('.islandora-compound-thumbs-wrapper, .islandora-compound-jail-thumbs').width();
+        var wrapLeft = jQuery('.islandora-compound-thumbs-wrapper, .islandora-compound-jail-thumbs').offset().left;
+        if (activeLeft > (wrapLeft + wrapWidth)){
+          jQuery(active).scrollParent().animate({'scrollLeft': (activeLeft - wrapLeft - (wrapWidth / 2)) + 'px'},500);
+        }
       }
-    }
-  });
 
-  // Show and hide captions from new grid display.
-  jQuery(window).load(function(){
-    if (jQuery('.islandora-newspaper-issue, .solr-grid-thumb, .islandora-basic-collection-thumb').length > 0) {
-      jQuery('.solr-grid-thumb, .solr-grid-caption, .islandora-basic-collection-thumb, .islandora-basic-collection-caption, .islandora-newspaper-issue .islandora-object-thumb, .islandora-newspaper-issue .islandora-object-caption').mouseover(function(){
-        jQuery(this).parent().children('.solr-grid-caption, .islandora-basic-collection-caption, .islandora-object-caption').show();
-      }).mouseout(function(){
-        jQuery(this).parent().children('.solr-grid-caption, .islandora-basic-collection-caption, .islandora-object-caption').hide();
+      // Show and hide captions from new grid display.
+      if (jQuery('.islandora-newspaper-issue, .solr-grid-thumb, .islandora-basic-collection-thumb').length > 0) {
+        jQuery('.solr-grid-thumb, .solr-grid-caption, .islandora-basic-collection-thumb, .islandora-basic-collection-caption, .islandora-newspaper-issue .islandora-object-thumb, .islandora-newspaper-issue .islandora-object-caption').mouseover(function(){
+          jQuery(this).parent().children('.solr-grid-caption, .islandora-basic-collection-caption, .islandora-object-caption').show();
+        }).mouseout(function(){
+          jQuery(this).parent().children('.solr-grid-caption, .islandora-basic-collection-caption, .islandora-object-caption').hide();
+        });
+      }
+
+      // Toggle Dental information box if exists.
+      jQuery('.uofm-dental-info h2', context).click(function(){ 
+        jQuery(this).next('table').toggle(function(){ 
+          var tmpT = (jQuery(this).is(':visible') ? 'hide' : 'show');
+          jQuery(this).prev('h2').text('Information (' + tmpT + ')');
+        });
       });
+
     }
-  });
+  };
 
 })(jQuery, Drupal, this, this.document);
