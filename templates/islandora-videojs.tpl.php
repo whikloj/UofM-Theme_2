8248133ef93febb5a4f194e09ed088fba6c81863
Jared Whiklo <jwhiklo@gmail.com>
2017-01-24 15:36:29 -0600

    Modify videojs player template

---
+++ templates/islandora-videojs.tpl.php
@@ -0,0 +1,7 @@
+<video id="islandora_videojs" class="video-js vjs-default-skin" controls
+       preload="auto" width="100%" height="264" poster="<?php print $tn; ?>"
+       data-setup="{}">
+  <?php foreach ($sources as $source): ?>
+    <source src="<?php print $source['url']; ?>" type='<?php print $source['mime']; ?>'>
+  <?php endforeach; ?>
+</video>
